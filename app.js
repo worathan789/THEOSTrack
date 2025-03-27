505df122143a89ab7b78da70fad4017e18010544
worathan789 <worathan.wic@gmail.com>
2025-03-27 12:02:22 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,61 +1,62 @@
-
 const viewer = new Cesium.Viewer('cesiumContainer', {
+    shouldAnimate: true,
     timeline: true,
     animation: true
 });
 
-function loadSatellites() {
-    const tle1 = [
-        "THEOS-1",
-        "1 99999U 23001A   24084.00000000  .00000000  00000-0  00000-0 0 0000",
-        "2 99999 098.7820 000.0000 0001000 000.0000 000.0000 14.80000000    01"
-    ];
-    const tle2 = [
-        "THEOS-2",
-        "1 99998U 23002A   24084.00000000  .00000000  00000-0  00000-0 0 0000",
-        "2 99998 098.7820 000.0000 0001000 000.0000 000.0000 14.80000000    02"
-    ];
+// TLE ต้นฉบับ (THEOS-1)
+const tle1 = [
+    "1 99999U 23001A   25084.00000000  .00000000  00000-0  00000-0 0 00001",
+    "2 99999 098.7000 000.0000 0001000 000.0000 000.0000 14.20000000    01"
+];
 
-    function addSatellite(tle, color) {
-        const satrec = satellite.twoline2satrec(tle[1], tle[2]);
-        const totalMinutes = 1440;
-        const positionsOverTime = new Cesium.SampledPositionProperty();
-        const start = Cesium.JulianDate.now();
-        for (let i = 0; i < totalMinutes; i += 10) {
-            const time = Cesium.JulianDate.addMinutes(start, i, new Cesium.JulianDate());
-            const date = Cesium.JulianDate.toDate(time);
-            const pos = satellite.propagate(satrec, date);
-            if (pos.position) {
-                const gmst = satellite.gstime(date);
-                const p = satellite.eciToGeodetic(pos.position, gmst);
-                const carto = Cesium.Cartographic.fromDegrees(Cesium.Math.toDegrees(p.longitude), Cesium.Math.toDegrees(p.latitude), p.height * 1000);
-                const pos3d = Cesium.Cartesian3.fromRadians(carto.longitude, carto.latitude, carto.height);
-                positionsOverTime.addSample(time, pos3d);
-            }
-        }
-        viewer.entities.add({
-            name: tle[0],
-            position: positionsOverTime,
-            point: { pixelSize: 6, color: color },
-            label: {
-                text: tle[0],
-                font: '14pt sans-serif',
-                fillColor: color,
-                style: Cesium.LabelStyle.FILL_AND_OUTLINE,
-                outlineWidth: 2,
-                verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
-                pixelOffset: new Cesium.Cartesian2(0, -9)
-            },
-            path: {
-                resolution: 600,
-                material: color,
-                width: 2
-            }
-        });
+const tle2 = [
+    "1 88888U 23002A   25084.00000000  .00000000  00000-0  00000-0 0 00002",
+    "2 88888 098.7000 005.0000 0001000 000.0000 000.0000 14.25000000    02"
+];
+
+function propagateTLE(tle, color, name) {
+    const satrec = satellite.twoline2satrec(tle[0], tle[1]);
+    const positions = [];
+    const start = Cesium.JulianDate.fromDate(new Date());
+    for (let i = 0; i < 90; i++) {
+        const time = Cesium.JulianDate.addMinutes(start, i * 5, new Cesium.JulianDate());
+        const date = Cesium.JulianDate.toDate(time);
+        const positionAndVelocity = satellite.propagate(satrec, date);
+        const positionEci = positionAndVelocity.position;
+        if (!positionEci) continue;
+        const gmst = satellite.gstime(date);
+        const positionGd = satellite.eciToGeodetic(positionEci, gmst);
+        const longitude = Cesium.Math.toDegrees(positionGd.longitude);
+        const latitude = Cesium.Math.toDegrees(positionGd.latitude);
+        const height = positionGd.height * 1000;
+        const cartesian = Cesium.Cartesian3.fromDegrees(longitude, latitude, height);
+        positions.push(cartesian);
     }
 
-    addSatellite(tle1, Cesium.Color.BLUE);
-    addSatellite(tle2, Cesium.Color.CYAN);
+    viewer.entities.add({
+        name: name,
+        polyline: {
+            positions: positions,
+            width: 2,
+            material: color
+        },
+        description: "Orbit of " + name
+    });
 }
 
-loadSatellites();
+document.getElementById("theos1").addEventListener("change", (e) => {
+    viewer.entities.removeAll();
+    if (e.target.checked) propagateTLE(tle1, Cesium.Color.SKYBLUE, "THEOS-1");
+    if (document.getElementById("theos2").checked) propagateTLE(tle2, Cesium.Color.CYAN, "THEOS-2");
+});
+
+document.getElementById("theos2").addEventListener("change", (e) => {
+    viewer.entities.removeAll();
+    if (document.getElementById("theos1").checked) propagateTLE(tle1, Cesium.Color.SKYBLUE, "THEOS-1");
+    if (e.target.checked) propagateTLE(tle2, Cesium.Color.CYAN, "THEOS-2");
+});
+
+// load default
+propagateTLE(tle1, Cesium.Color.SKYBLUE, "THEOS-1");
+propagateTLE(tle2, Cesium.Color.CYAN, "THEOS-2");
